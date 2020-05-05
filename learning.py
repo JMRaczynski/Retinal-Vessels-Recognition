d38b26eb918e876bc7014f61a3db7aeca65601de
happyberry <j.tadej@wp.pl>
2020-05-05 20:26:38 +0200

    gridsearchCV

--- learning.py
+++ learning.py
@@ -6,15 +6,14 @@ from sklearn import tree
 from sklearn.model_selection import cross_val_score, GridSearchCV
 import joblib
 from skimage import data, exposure, io, color, img_as_float, filters
+import random
 
 def addImage(dataFilename, maskFilename, pixelPerCell):
     countTrue, countFalse = 0, 0
     dataIm = np.array(Image.open(dataFilename)) #you can pass multiple arguments in single line\n",
-    gamma=1.5# Gamma < 1 ~ Dark ; Gamma > 1 ~ Bright
-    dataIm=((dataIm/255)**(1/gamma))
     maskIm = np.array(Image.open(maskFilename)) #you can pass multiple arguments in single line\n",
     dataGrey = dataIm[:,:,:]
-    #dataGrey[:,:,0] = 0 # PREPROCESSING
+    dataGrey[:,:,0] = 0 # PREPROCESSING
     dataGrey[:,:,2] = 0
     dataGrey = color.rgb2gray(dataGrey)
     dataGrey = exposure.equalize_adapthist(dataGrey)
@@ -25,45 +24,47 @@ def addImage(dataFilename, maskFilename, pixelPerCell):
     #dataGrey = filters.gaussian(dataGrey, sigma=2)
     margin = pixelPerCell-pixelPerCell//2
     translation = pixelPerCell//2
-    for i in range(margin, maskIm.shape[0] - margin - 1, 3):
-        for j in range(margin, maskIm.shape[1] - margin - 1):#, translation-1):
-            if (((i - 477) ** 2) + ((j - 494) ** 2)) ** 0.5 > 465:
-                continue
-            if maskIm[i][j] == False and countFalse > 2*countTrue:
-                continue
-            subData = [] #tablica na parametry obliczone dla komórki\n",
+    while countTrue < 50000:
+        i = random.randrange(15, dataIm.shape[0] - 15)
+        j = random.randrange(15, dataIm.shape[1] - 15)
+        if (((i - 477) ** 2) + ((j - 494) ** 2)) ** 0.5 > 475:
+            continue
+        if maskIm[i][j] == False and countFalse > countTrue:
+            continue
+        subData = [] #tablica na parametry obliczone dla komórki\n",
         #print(positionX, positionY)\n",
         #***skopiowanie fragmentu obrazu\n",
-            subImage = dataIm[i-translation:i+translation+1,j-translation:j+translation+1,:]
-            varianceR = ndimage.measurements.variance(subImage[:, :, 0])
-            varianceG = ndimage.measurements.variance(subImage[:, :, 1])
-            subData.append(varianceR)
-            subData.append(varianceG)
-            #stat = ImageStat.Stat(Image.fromarray(subImage).convert('L'))
-            #subData.append(stat.mean[0])
-            #***PREPROCESSING
-            subImage = dataGrey[i-translation:i+translation+1,j-translation:j+translation+1]
-            #***obliczanie momentów Hu
-            moments = cv2.moments(subImage)
-            huMoments = cv2.HuMoments(moments)
-            #centralMoments = cv2.central_moments(subImage)
-            #print(moments)\n",
-            #print(huMoments)\n",
-            #cy, cx = ndimage.center_of_mass(subImage)
-            #subData.extend((cy, cx))
-            #for m in huMoments:
-            #    subData.append(m[0])
-            subData.extend((huMoments[0][0], huMoments[3][0], huMoments[6][0]))
-            #print(subData)\n",
-            #***zapisanie zgromadzonych danych\n",
-            if subData not in data['data']:
-                data['data'].append(subData)
-                    #***dodanie decyzji z maski eksperckiej\n",
-                data['mask'].append(maskIm[i][j])
-                if maskIm[i][j] == True:
-                    countTrue += 1
-                else:
-                    countFalse += 1
+        subImage = dataIm[i-translation:i+translation+1,j-translation:j+translation+1,:]
+        #varianceR = ndimage.measurements.variance(subImage[:, :, 0])
+        varianceG = ndimage.measurements.variance(subImage[:, :, 1])
+        #subData.append(varianceR)
+        subData.append((dataIm[i][j][0]+dataIm[i][j][1] + dataIm[i][j][2])/3)
+        subData.append(varianceG)
+        #stat = ImageStat.Stat(Image.fromarray(subImage).convert('L'))
+        #subData.append(stat.mean[0])
+        #***PREPROCESSING
+        subImage = dataGrey[i-translation:i+translation+1,j-translation:j+translation+1]
+        #***obliczanie momentów Hu
+        moments = cv2.moments(subImage)
+        huMoments = cv2.HuMoments(moments)
+        #centralMoments = cv2.central_moments(subImage)
+        #print(moments)\n",
+        #print(huMoments)\n",
+        #cy, cx = ndimage.center_of_mass(subImage)
+        #subData.extend((cy, cx))
+        # #for m in huMoments:
+        #    subData.append(m[0])
+        subData.extend((huMoments[0][0], huMoments[3][0], huMoments[6][0]))
+        #print(subData)\n",
+        # #***zapisanie zgromadzonych danych\n",
+        #if subData not in data['data']:
+        data['data'].append(subData)
+        #***dodanie decyzji z maski eksperckiej\n",
+        data['mask'].append(maskIm[i][j])
+        if maskIm[i][j] == True:
+            countTrue += 1
+        else:
+            countFalse += 1
 
 
 data = {}
@@ -71,16 +72,21 @@ data['data'] = []
 data['mask'] = []
 
 pixelPerCell = 9
-addImage('Image_01L.jpg', 'Image_01L_1stHO.png', pixelPerCell)
-#addImage('Image_04L.jpg', 'Image_04L_1stHO.png', pixelPerCell)
-addImage('Image_04R.jpg', 'Image_04R_1stHO.png', pixelPerCell)
+addImage('Image_02L.jpg', 'Image_02L_1stHO.png', pixelPerCell)
+#addImage('Image_03L.jpg', 'Image_03L_1stHO.png', pixelPerCell)
+addImage('Image_04L.jpg', 'Image_04L_1stHO.png', pixelPerCell)
+addImage('Image_06L.jpg', 'Image_06L_1stHO.png', pixelPerCell)
+#addImage('Image_07L.jpg', 'Image_07L_1stHO.png', pixelPerCell)
+#addImage('Image_12L.jpg', 'Image_12L_1stHO.png', pixelPerCell)
+#addImage('Image_14L.jpg', 'Image_14L_1stHO.png', pixelPerCell)
+print("OLADDED")
 
 x = data['data']
 y = data['mask']
-parameters = {'max_depth':range(4,8)}#, 'max_features':range(3,7)}
-clf = GridSearchCV(tree.DecisionTreeClassifier(), parameters, n_jobs=-1, scoring=['recall', 'accuracy', 'precision'], refit = 'accuracy')
+parameters = {'max_depth':range(5,11), 'max_features':range(3,6), 'criterion': ['gini', 'entropy']}
+clf = GridSearchCV(tree.DecisionTreeClassifier(), parameters, n_jobs=-1, scoring=['recall', 'accuracy', 'precision'], refit = 'precision')
 clf.fit(X=x, y=y)
 tree_model = clf.best_estimator_
 print (clf.best_score_, clf.best_params_)
 
-joblib.dump(tree_model, "drzewko1.pkl")
+joblib.dump(tree_model, "drzewko500.pkl")
